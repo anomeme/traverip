0d7ed5effa46e575794630cfa76a3ca56ac42ec0
anomeme <x.music.sky@gmail.com>
2019-12-07 01:36:49 +0900

    article-list_create

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'articles#index'
   resources :users, only:[:show, :edit]
-  resources :articles
+  resources :articles do
+    collection do
+      get 'list'
+      get 'list_search', defaults: { format: 'json' }
+    end
+  end
   resources :pictures do
     collection do
       get 'main_image', defaults: { format: 'json' }
