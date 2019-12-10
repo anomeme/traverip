9ddbc0a3ef91b9a3cda01b7c599e38ec6d5d889f
anomeme <x.music.sky@gmail.com>
2019-12-10 14:37:59 +0900

    basic_check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :set_search
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth, if: :production?
+  protect_from_forgery with: :exception  
 
   def set_search
     words = params[:q].delete(:title_or_detail_cont) if params[:q].present?
@@ -17,7 +19,18 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
 end
